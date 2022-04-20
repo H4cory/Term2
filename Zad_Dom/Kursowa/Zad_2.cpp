ae3e982e4c2e630053757deb55d9ad9843f7fdda
H4cory <k.kanev13@gmail.com>
2022-04-20 16:51:36 +0300

    20.04.2022

---
+++ Zad_Dom/Kursowa/Zad_2.cpp
@@ -0,0 +1,20 @@
+/*
+V gradinski tsentar se prodavat rasteniya (naimenovanie, tsena, broy),
+koito bivat darveta (visochina, vid(iglolistni/shirokolistni)) i tsvetya
+(tsvyat). Dannite za vsyako rastenie se sahranyava v masiv.
+Da se sastavi programa, koyato izpalnyava ot menyu slednite operatsii:
+-       dobavya kam masiva dannite na novo rastenie;
+-       izvezhda vsichki vavedeni danni;
+-       izvezhda rastenieto s nay-visoka tsena;
+-       sazdava masiv s naimenovaniyata na chervenite tsvetya.
+*/
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(){
+ 
+   return 0;
+}
