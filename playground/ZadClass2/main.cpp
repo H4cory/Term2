d94bb5adaa4729b71f66989adb44c14ea2474c70
H4cory <k.kanev13@gmail.com>
2022-05-24 19:16:28 +0300

    24.05.2022

---
+++ playground/ZadClass2/main.cpp
@@ -0,0 +1,9 @@
+/*
+V gradinski tsentar se prodavat rasteniya (naimenovanie, tsena, broy), koito bivat darveta (visochina,
+vid(iglolistni/shirokolistni)) i tsvetya (tsvyat). Dannite za vsyako rastenie se sahranyava v masiv.
+Da se sastavi programa, koyato izpalnyava ot menyu slednite operatsii:
+-    dobavya kam masiva dannite na novo rastenie;
+-    izvezhda vsichki vavedeni danni;
+-    izvezhda danni za iglolistnite darveta;
+-    sazdava masiv s dannite na rasteniyata s tsena pod 10lv.
+*/
