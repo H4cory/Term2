5903d249a8d29d1ee9edf3cbe17326b713ad02cc
H4cory <93988385+H4cory@users.noreply.github.com>
2022-06-01 19:33:18 +0300

    Update Izpit.cpp

--- Zad_Dom/Kursowa/Izpit.cpp
+++ Zad_Dom/Kursowa/Izpit.cpp
@@ -1,3 +1,23 @@
 /*
+1. Da se sastavi klas CHouse, koyto sahranyava informatsiya za kashti:
+-adres tsena
+-broy stai Klasat tryabva da vklyuchva:
+-konstruktor bez argumenti:
+-konstruktor, initsializirasht vsichki chlen-danni na klasa;
+-chlen-funktsiite za vavezhdane i izvezhdane na dannite za edna kashta. chlen-funktsiya, koyato proveryava dali kashtata e s poveche stai ot zadadena STOYNOST.
+
+Da se napishe programa, koyato:
+-vavezhda v masiv danni za nepoveche ot 100 kashti;
+-izvezhda spisak s kashtite, koyto sa po-poveche ot 10 stai.
+
+
+
+2. V magazin za mebeli se predlagat mebeli (naimenovanie i tsena), kato te bivat stolove (naimenovanie, tsena, tsvyat) 
+i masi (naimenovanie, tsena, broy mesta na masata) Dannite da se sahranyavat v edna obshta dannova struktura, naprimer podhodyasht masiv (polimorfizam).
+
+-Da se sastavi programa, koyato izpalnyava ot menyu slednite operatsii: masa;
+-dobavya kam masiva danni za stol ili izvezhda vsICHKI VAVEDeni danni;
+- iztriva danni za mebel po zadadeno naimenovanie; izvezhda dannite za vsichki beli stolove;
+-sazdava masiv s dannite na vsichki masi za chetirima.
 
 */
