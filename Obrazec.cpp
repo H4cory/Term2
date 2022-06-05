254253839676d584ddebe73fb650c3d80fc65bd0
H4cory <k.kanev13@gmail.com>
2022-06-05 14:23:31 +0300

    04.05.2022

--- Obrazec.cpp
+++ Obrazec.cpp
@@ -2,11 +2,19 @@
 
 */
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main(){
+   string a = "0148255287";
+  // getline(cin,a);
+  if(stoi(a.substr(2,2)) >  12){
+   cout<<stoi(a.substr(2,2))-40;
+   }
+   else
+   cout<<stoi(a.substr(2,2));
 
  
  
